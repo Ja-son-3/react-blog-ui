7e263fa9a29a7cac509f1ad4aa6cf59084371458
Jason Huang <jason97521@gmail.com>
2023-03-09 11:26:00 -0500

    blog post component

---
+++ src/post/Post.jsx
@@ -0,0 +1,21 @@
+import "./post.css"
+
+export default function post() {
+  return (
+    <div className="post">
+        <img className="postImg" src="https://images.unsplash.com/photo-1612441804231-77a36b284856?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bW91bnRhaW4lMjBsYW5kc2NhcGV8ZW58MHx8MHx8&w=1000&q=80" alt="" />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">
+                Lorem, ipsum dolor sit 
+            </span>
+            <hr />
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className="postDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat dicta eos placeat nemo quoss. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat dicta eos placeat nemo quoss. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat dicta eos placeat nemo quoss. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat dicta eos placeat nemo quoss. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat dicta eos placeat nemo quoss. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat dicta eos placeat nemo quoss. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat dicta eos placeat nemo quoss. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat dicta eos placeat nemo quoss. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat dicta eos placeat nemo quoss. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat dicta eos placeat nemo quoss. </p>
+    </div>
+  )
+}
